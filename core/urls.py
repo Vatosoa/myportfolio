102de92aa0abcb8c718cb8276ecbbcf8616b82bd
Vatosoa Razafindrazaka <razafindrazakav@gmail.com>
2025-08-05 07:01:00 +0200

    [DELETE, UPDATE] All views not important

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('research/', views.research, name='research'),
+    path('experience/', views.experience, name='experience'),
+    path('projects/', views.projects, name='projects'), 
     path('publications/', views.publications, name='publications'),
     path('cv/', views.cv, name='cv'),
-    path('projects/', views.projects, name='projects'), 
-    path('blog/', views.blog, name='blog'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,20 +5,14 @@ from django.utils.timezone import now  # pour automatiser l'année
 def about(request):
     return render(request, 'core/about.html', { 'now': now() })
 
-def contact(request):
-    return render(request, 'core/contact.html', { 'now': now() })
+def experience(request):
+    return render(request, 'core/experience.html', { 'now': now() })
 
-def research(request):
-    return render(request, 'core/research.html', { 'now': now() })
+def projects(request):
+    return render(request, 'core/projects.html', { 'now': now() })
 
 def publications(request):
     return render(request, 'core/publications.html', { 'now': now() })
 
 def cv(request):
     return render(request, 'core/cv.html', { 'now': now() })
-
-def projects(request):
-    return render(request, 'core/projects.html', { 'now': now() })
-
-def blog(request):
-    return render(request, 'core/blog.html', { 'now': now() })
