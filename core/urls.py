d4748c1849e4d5b86792a560fa6367fe8f164470
Vatosoa <razafindrazakav@gmail.com>
2026-01-11 21:27:23 +0100

    [FIX] Fix error lang FR/EN

--- core/urls.py
+++ core/urls.py
@@ -1,45 +1,20 @@
 # core/urls.py
 from django.urls import path
-from django_distill import distill_path, distill_re_path
+from django_distill import distill_path
 from . import views
-from django.conf import settings
 
-# Fonction pour générer les langues disponibles
-def get_languages():
-    for lang_code, _ in settings.LANGUAGES:
-        yield {'lang': lang_code}
-
-# Fonction pour chaque vue avec langue
-def get_home_urls():
-    for lang_code, _ in settings.LANGUAGES:
-        yield {'lang': lang_code}
-        # Page d'accueil par défaut sans langue
-        if lang_code == 'en':  # Par défaut en anglais
-            yield {'lang': ''}
+# Fonction simple pour itérer sur les langues
+def get_all_languages():
+    # Retourne juste une liste vide car i18n_patterns gère l'argument 'lang'
+    # Mais distill a besoin d'un dictionnaire vide par page pour confirmer la génération
+    yield {}
 
 urlpatterns = [
-    # Pages avec support multi-langue
-    distill_path('', views.home, name='home', 
-                distill_file='{lang}/index.html',
-                distill_func=get_home_urls),
-    
-    distill_path('about/', views.about, name='about',
-                distill_file='{lang}/about/index.html',
-                distill_func=get_languages),
-    
-    distill_path('projects/', views.projects, name='projects',
-                distill_file='{lang}/projects/index.html',
-                distill_func=get_languages),
-    
-    distill_path('experience/', views.experience, name='experience',
-                distill_file='{lang}/experience/index.html',
-                distill_func=get_languages),
-    
-    distill_path('publications/', views.publications, name='publications',
-                distill_file='{lang}/publications/index.html',
-                distill_func=get_languages),
-    
-    distill_path('cv/', views.cv, name='cv',
-                distill_file='{lang}/cv/index.html',
-                distill_func=get_languages),
+    # Ne mettez PAS {lang} ici, i18n_patterns s'en occupe
+    distill_path('', views.home, name='home', distill_func=get_all_languages),
+    distill_path('about/', views.about, name='about', distill_func=get_all_languages),
+    distill_path('projects/', views.projects, name='projects', distill_func=get_all_languages),
+    distill_path('experience/', views.experience, name='experience', distill_func=get_all_languages),
+    distill_path('publications/', views.publications, name='publications', distill_func=get_all_languages),
+    distill_path('cv/', views.cv, name='cv', distill_func=get_all_languages),
 ]
--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for portfolio_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
@@ -27,21 +11,14 @@ def redirect_to_language(request):
     lang = translation.get_language_from_request(request, check_path=False)
     return HttpResponseRedirect(f'/{lang}/')
 
-# Redirige automatiquement vers /en/ ou /fr/ selon la langue du navigateur
-# def redirect_to_language(request):
-#     lang = translation.get_language_from_request(request)
-#     return HttpResponseRedirect(f'/{lang}/')
-
+# portfolio_site/urls.py
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('', redirect_to_language),  # 👈 redirection ajoutée ici
-]
-
-urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-)
-
+    path('i18n/', include('django.conf.urls.i18n')),
+    # Supprimez i18n_patterns pour la génération statique si possible
+    # OU gardez-le mais assurez-vous que core.urls ne demande pas d'arguments
+    path('', include('core.urls')), 
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
