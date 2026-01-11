23a548004f477de8cfc7aca5765f53191906b054
Vatosoa <razafindrazakav@gmail.com>
2026-01-11 22:23:39 +0100

    Fix : delete other files

--- core/urls.py
+++ core/urls.py
@@ -14,12 +14,5 @@ urlpatterns = [
     # La vue 'home' (qui utilise base.html + index.html de Django) 
     # doit devenir un fichier physique 'index.html' pour GitHub
     distill_path('', views.home, name='home', distill_file='index.html'),
-
-    # La vue 'about' doit devenir 'about/index.html'
-    distill_path('about/', views.about, name='about', distill_file='about/index.html'),
-    distill_path('projects/', views.projects, name='projects', distill_file='projects/index.html'),
-    distill_path('experience/', views.experience, name='experience', distill_file='experience/index.html'),
-    distill_path('publications/', views.publications, name='publications', distill_file='publications/index.html'),
-    distill_path('cv/', views.cv, name='cv', distill_file='cv/index.html'),
 ]
 
