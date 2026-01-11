52d524388d0f74ab654ca9d04326d1b91e76dc27
Vatosoa <razafindrazakav@gmail.com>
2026-01-11 22:19:35 +0100

    Fix : autre test sur les redirections

--- core/urls.py
+++ core/urls.py
@@ -8,13 +8,18 @@ def get_all_languages():
     # Retourne juste une liste vide car i18n_patterns gère l'argument 'lang'
     # Mais distill a besoin d'un dictionnaire vide par page pour confirmer la génération
     yield {}
-
+    
+# core/urls.py
 urlpatterns = [
-    # Ne mettez PAS {lang} ici, i18n_patterns s'en occupe
-    distill_path('', views.home, name='home', distill_func=get_all_languages),
-    distill_path('about/', views.about, name='about', distill_func=get_all_languages),
-    distill_path('projects/', views.projects, name='projects', distill_func=get_all_languages),
-    distill_path('experience/', views.experience, name='experience', distill_func=get_all_languages),
-    distill_path('publications/', views.publications, name='publications', distill_func=get_all_languages),
-    distill_path('cv/', views.cv, name='cv', distill_func=get_all_languages),
+    # La vue 'home' (qui utilise base.html + index.html de Django) 
+    # doit devenir un fichier physique 'index.html' pour GitHub
+    distill_path('', views.home, name='home', distill_file='index.html'),
+
+    # La vue 'about' doit devenir 'about/index.html'
+    distill_path('about/', views.about, name='about', distill_file='about/index.html'),
+    distill_path('projects/', views.projects, name='projects', distill_file='projects/index.html'),
+    distill_path('experience/', views.experience, name='experience', distill_file='experience/index.html'),
+    distill_path('publications/', views.publications, name='publications', distill_file='publications/index.html'),
+    distill_path('cv/', views.cv, name='cv', distill_file='cv/index.html'),
 ]
+
